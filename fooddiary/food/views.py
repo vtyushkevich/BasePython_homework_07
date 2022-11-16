7e3991c8da2c1b2e1cddda5d853c3f5061f71bc3
vadim <vadim.tyushkevich@yandex.ru>
2022-11-17 00:19:45 +0500

    Add food templates

--- fooddiary/food/views.py
+++ fooddiary/food/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 
 def index(request: HttpRequest):
 
-    return HttpResponse(f"<h1>Hello path <code>{request.path}</code><h1>")
+    # return HttpResponse(f"<h1>Hello path <code>{request.path}</code><h1>")
+    return render(request=request, template_name="food/index.html")
